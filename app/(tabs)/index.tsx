0c6d9f69500a5e5eecdafb77d7cda1c9cebefd59
techbysundaram <techbysundaram@gmail.com>
2025-11-05 17:21:58 +0530

    new style files added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,11 @@
-import useTheme from "@/hooks/useTheme";
-import { Text, TouchableOpacity, View } from "react-native";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const styles = createStyles(colors);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle for change mode</Text>
@@ -18,3 +13,19 @@ export default function Index() {
     </View>
   );
 }
+
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 10,
+      backgroundColor: colors.bg,
+    },
+    content: {
+      fontSize: 22,
+    },
+  });
+  return styles;
+};
